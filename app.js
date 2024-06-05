08c26eb993e10cc12fbd4881d838e38314143c07
Ravi <raviiitg2020@gmail.com>
2024-06-05 11:41:53 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -9,13 +9,20 @@ connectDB();
 
 const app = express();
 
+// Allow requests from specific origin
+app.use(
+  cors({
+    origin: "https://movie-library-frontend.netlify.app",
+  })
+);
+app.use(express.json());
 
-app.get('/',(req, res) => {
-    res.status(201).json("server started");
+app.get("/", (req, res) => {
+  res.status(201).json("server started");
 });
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api', require('./routes/listRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/listRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
